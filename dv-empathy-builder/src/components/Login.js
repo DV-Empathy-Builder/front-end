838c9bf517aee6538d4c4ff3b92b51bc8d93bb75
astillo <clomes.alex@gmail.com>
2019-08-26 12:27:02 -0500

    login and register done

---
+++ dv-empathy-builder/src/components/Login.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Form as Formik, Field, withFormik } from 'formik'
+import { Segment, Form, Button } from 'semantic-ui-react'
+import axios from 'axios'
+
+const Login = props => {
+  return (
+    <Segment raised compact>
+      <Form>
+        <Formik>
+          <Form.Field>
+            <Field type="text" name="username" placeholder="username" />
+          </Form.Field>
+          <Form.Field>
+            <Field type="password" name="password" placeholder="password" />
+          </Form.Field>
+          <Button type="submit">Login</Button>
+        </Formik>
+      </Form>
+    </Segment>
+  )
+}
+
+const FormikForm = withFormik({
+  mapPropsToValues(values) {
+    return {
+      username: values.username || '',
+      password: values.password || ''
+    }
+  },
+  handleSubmit(values, props) {
+    axios.post('dv-empathy.herokuapp.com/auth/login', values)
+      .then(res => {
+        localStorage.setItem('token', res.data.payload)
+      })
+      .then(res => setTimeout(() => {
+        props.props.history.push('/')
+      }, 1000))
+      .catch(err => console.log(err))
+  }
+})(Login)
+
+export default FormikForm
---
+++ dv-empathy-builder/src/components/Register.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Form as Formik, Field, withFormik } from 'formik'
+import { Segment, Form, Button } from 'semantic-ui-react'
+import axios from 'axios'
+
+const Register = props => {
+  return (
+    <Segment raised compact>
+      <Form>
+        <Formik>
+          <Form.Field>
+            <Field type="text" name="username" placeholder="Please enter a username" />
+          </Form.Field>
+          <Form.Field>
+            <Field type="password" name="password" placeholder="Please enter your password" />
+          </Form.Field>
+          <Button type="submit">Register</Button>
+        </Formik>
+      </Form>
+    </Segment>
+  )
+}
+
+const FormikForm = withFormik({
+  mapPropsToValues(values) {
+    return {
+      username: values.username || '',
+      password: values.password || ''
+    }
+  },
+  handleSubmit(values, props) {
+    axios.post('dv-empathy.herokuapp.com/auth/register', values)
+      .then(res => {
+        //localStorage.setItem('token', res.data.payload)
+        alert('Account was created. You will be redirected to login.')
+      })
+      .then(res => setTimeout(() => {
+        props.props.history.push('/login')
+      }, 1000))
+      .catch(err => console.log(err))
+  }
+})(Register)
+
+export default FormikForm
