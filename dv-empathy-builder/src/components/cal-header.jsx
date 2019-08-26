4c95717b111b988a764d2377c9e6ebc397522b1b
Daniel Jones <daniel.jones2000@gmail.com>
2019-08-26 10:42:01 -0700

    added formik and some more components

---
+++ dv-empathy-builder/src/components/cal-header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const CalHeader = () => {
+    return(
+        <header>
+            <h2>Fleeing a domestic violence situation is costly. Survivors can lose thier community, home, or financial security.</h2>
+        </header>
+    )
+}
