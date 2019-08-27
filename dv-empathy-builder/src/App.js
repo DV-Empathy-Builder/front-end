4cc975a45c2236e2b53a1b7cf540a60beb9106f3
Daniel Jones <daniel.jones2000@gmail.com>
2019-08-27 14:47:02 -0700

    filtered data

--- dv-empathy-builder/src/App.js
+++ dv-empathy-builder/src/App.js
@@ -1,32 +1,19 @@
-// import React from 'react';
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-
+import React from 'react';
 import './App.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Login from './Container/Login';
 import Register from './Container/Register';
 import PrivateRoute from './components/PrivateRoute';
 import Dashboard from './Container/Dashboard';
-
 import 'semantic-ui-css/semantic.min.css';
-import {axiosWithAuth} from './utils/axiosWithAuth';
+import CalculatorForm from './components/CalculatorForm';
 
 
 function App() {
-  const [CategoriesState, SetCategories] = useState([])
-  useEffect(() => {
-    axiosWithAuth()
-    .get("https://dv-empathy.herokuapp.com/categories/")
-    .then(res => {
-      console.log("Cat", res.data);
-      SetCategories(res.data);
-    })
-    .catch(err => console.log(err.response));
-  }, []);
   return (
     <Router>
       <div className="App">
+        <CalculatorForm/>
         <Route exact path="/" component={Dashboard}/>
         <Route exact path="/login" component={Login}/>
         <Route exact path='/signup' component={Register}/>
--- dv-empathy-builder/src/components/CalculatorForm.jsx
+++ dv-empathy-builder/src/components/CalculatorForm.jsx
@@ -1,15 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 // import { Button, Divider, Form } from 'semantic-ui-react'
-import { Form, Field, withFormik } from "formik";
+import { Form, Field, withFormik, Formik } from "formik";
+import axios from "axios";
+import {axiosWithAuth} from '../utils/axiosWithAuth';
+import BudgetForm from './BudgetForm';
 
+const CalculatorForm = () => {
+    const [CategoriesState, SetCategories] = useState([])
+    useEffect(() => {
+      axiosWithAuth()
+      .get("https://dv-empathy.herokuapp.com/categories/")
+      .then(res => {
+        console.log("Categories", res.data);
+        SetCategories(res.data);
+      })
+      .catch(err => console.log(err.response));
+    }, []);
+    if(!CategoriesState)return <h3>loading...</h3>;
+    
+    const personalBudget =  CategoriesState.filter(function(cat) {
+        return cat.category_type == "Personal";
+    });
+    const relocationBudget =  CategoriesState.filter(function(cat) {
+        return cat.category_type == "Relocation";
+    });
+    console.log(personalBudget);
+    console.log(relocationBudget);
+    return(
+        <div className="budget-parent">
+            <div>
+                <h3>STEP ONE: PERSONAL MONTHLY BUDGET</h3>
+                <p>If none, enter $0 (Zero)</p>
+            </div>
 
-const MonthlyBudgetForm = () => (
-    <div className="budget-parent">
-        <div>
-        <h3>STEP ONE: PERSONAL MONTHLY BUDGET</h3>
-        <p>If none, enter $0 (Zero)</p>
+        {personalBudget.map((categories) => {
+          return(
+             <BudgetForm/>
+          )
+        })}
+   
         </div>
-        
-    </div>
-)
-export default MonthlyBudgetForm;
+    );    
+}
+
+export default CalculatorForm;
